9a26a2f1ca6059a573933548e0c0dd73b68617c5
dgstephenson <dgstephenson.econ@gmail.com>
2022-03-12 13:55:15 -0500

    lint smartdrag

--- public/assets/setup.js
+++ public/assets/setup.js
@@ -1,117 +1,117 @@
-var numPlayers = 3
-var timelineLength = numPlayers+10
-var tableWidth = numPlayers<7 ? 3500 : 5000
-var numBottomRowPlayers = Math.round(numPlayers/2)
-var numTopRowPlayers = numPlayers-numBottomRowPlayers
-var topRowOrigins = []
-var bottomRowOrigins = []
-for(i=0; i<numTopRowPlayers; i++) {
-  var alpha = (i+1)/(numTopRowPlayers+1)
-  var x = -tableWidth*alpha+tableWidth*(1-alpha)
-  topRowOrigins.push([x,-900])
+let numPlayers = 3
+let timelineLength = numPlayers + 10
+let tableWidth = numPlayers < 7 ? 3500 : 5000
+let numBottomRowPlayers = Math.round(numPlayers / 2)
+let numTopRowPlayers = numPlayers - numBottomRowPlayers
+let topRowOrigins = []
+let bottomRowOrigins = []
+for (i = 0; i < numTopRowPlayers; i++) {
+  var alpha = (i + 1) / (numTopRowPlayers + 1)
+  var x = -tableWidth * alpha + tableWidth * (1 - alpha)
+  topRowOrigins.push([x, -900])
 }
-for(i=0; i<numBottomRowPlayers; i++) {
-  var alpha = (i+1)/(numBottomRowPlayers+1)
-  var x = -tableWidth*alpha+tableWidth*(1-alpha)
-  bottomRowOrigins.push([x,900])
+for (i = 0; i < numBottomRowPlayers; i++) {
+  var alpha = (i + 1) / (numBottomRowPlayers + 1)
+  var x = -tableWidth * alpha + tableWidth * (1 - alpha)
+  bottomRowOrigins.push([x, 900])
 }
 origins = topRowOrigins.concat(bottomRowOrigins)
 deckCount = 0
 
 shuffle = array => {
-  let shuffled = array
+  const shuffled = array
     .map(item => ({ value: item, priority: Math.random() }))
     .sort((a, b) => a.priority - b.priority)
     .map(x => x.value)
   return shuffled
 }
 
-const describeRow = function(file,x,y,type,n,length,side='front') {
-  let descriptions = []
-  for(i=0; i<n; i++) {
-    var alpha = 0
-    if(n>1) alpha = i/(n-1)
-    var myX = (x-0.5*length)*(1-alpha) + (x+0.5*length)*alpha
-    description = window.client.describe({file:file,x:myX,y:y,type:type})
+const describeRow = function (file, x, y, type, n, length, side = 'front') {
+  const descriptions = []
+  for (i = 0; i < n; i++) {
+    let alpha = 0
+    if (n > 1) alpha = i / (n - 1)
+    let myX = (x - 0.5 * length) * (1 - alpha) + (x + 0.5 * length) * alpha
+    description = window.client.describe({ file: file, x: myX, y: y, type: type })
     descriptions.push(description)
   }
-  return(descriptions)
+  return (descriptions)
 }
 
-const describeColumn = function(file,x,y,type,n,length,side='front') {
-  let descriptions = []
-  for(i=0; i<n; i++) {
-    var alpha = 0
-    if(n>1) alpha = i/(n-1)
-    var myY = (y-0.5*length)*(1-alpha) + (y+0.5*length)*alpha
-    description = client.describe({file:file,x:x,y:myY,type:type,side:side})
+const describeColumn = function (file, x, y, type, n, length, side = 'front') {
+  const descriptions = []
+  for (i = 0; i < n; i++) {
+    let alpha = 0
+    if (n > 1) alpha = i / (n - 1)
+    let myY = (y - 0.5 * length) * (1 - alpha) + (y + 0.5 * length) * alpha
+    description = client.describe({ file: file, x: x, y: myY, type: type, side: side })
     descriptions.push(description)
   }
-  return(descriptions)
+  return (descriptions)
 }
 
-const describePortfolio = (x,y,player) => {
+const describePortfolio = (x, y, player) => {
   let descriptions = []
-  let sgn = Math.sign(y)
+  const sgn = Math.sign(y)
   offset = 0
   angle = 0
-  if(sgn==-1) {
+  if (sgn == -1) {
     angle = 180
     offset = 1
   }
   deckCount += 1
   descriptions = descriptions.concat([
-    client.describe({file:'board/ready',x:x+000,y:y+sgn*820,type:'screen',player:player}),
-    client.describe({file:'board/nametag',x:x+000,y:y+sgn*680,type:'board'}),
-    client.describe({file:'board/screen',x:x-000,y:y+sgn*400,type:'screen',rotation:angle,player:player}),
-    client.describe({file:'stack/discard',x:x-500,y:y+sgn*000,type:'discard',targetDeck:deckCount}),
-    client.describe({file:'stack/deck'   ,x:x+500,y:y+sgn*000,type:'deck',deckId:deckCount}),
-    client.describe({file:'board/playarea',x:x+000,y:y-sgn*400,type:'board'}),
+    client.describe({ file: 'board/ready', x: x, y: y + sgn * 820, type: 'screen', player: player }),
+    client.describe({ file: 'board/nametag', x: x, y: y + sgn * 680, type: 'board' }),
+    client.describe({ file: 'board/screen', x: x, y: y + sgn * 400, type: 'screen', rotation: angle, player: player }),
+    client.describe({ file: 'stack/discard', x: x - 500, y: y + sgn * 0, type: 'discard', targetDeck: deckCount }),
+    client.describe({ file: 'stack/deck' , x: x + 500, y: y + sgn * 0, type: 'deck', deckId: deckCount }),
+    client.describe({ file: 'board/playarea', x: x, y: y - sgn * 400, type: 'board' })
   ])
-  var yStack = sgn==1 ? -20 : -20
+  let yStack = sgn == 1 ? -20 : -20
   descriptions = descriptions.concat([
-    client.describe({file:'card/front',x:x-500,y:y-20,type:'card',cardId:11}),
-    client.describe({file:'card/front',x:x+500,y:y-20,type:'card',cardId:2,side:'facedown'}),
+    client.describe({ file: 'card/front', x: x - 500, y: y - 20, type: 'card', cardId: 11 }),
+    client.describe({ file: 'card/front', x: x + 500, y: y - 20, type: 'card', cardId: 2, side: 'facedown' })
   ])
-  for(i of Array(8).keys()) {
-    var cardId = i+3
+  for (i of Array(8).keys()) {
+    let cardId = i + 3
     descriptions = descriptions.concat([
-      client.describe({file:'card/front',x:x+(i-3.5)*120,y:y+sgn*400,type:'card',cardId:cardId})
+      client.describe({ file: 'card/front', x: x + (i - 3.5) * 120, y: y + sgn * 400, type: 'card', cardId: cardId })
     ])
   }
-  descriptions = descriptions.concat(describeRow('gold/1',x,y-sgn*100,'bit',5,300))
-  descriptions = descriptions.concat(describeRow('gold/5',x,y+sgn*50,'bit',3,300))
-  return(descriptions)
+  descriptions = descriptions.concat(describeRow('gold/1', x, y - sgn * 100, 'bit', 5, 300))
+  descriptions = descriptions.concat(describeRow('gold/5', x, y + sgn * 50, 'bit', 3, 300))
+  return (descriptions)
 }
 
-const describeBank = (x,y) => {
+const describeBank = (x, y) => {
   let descriptions = []
   descriptions = descriptions.concat([
-    client.describe({file:'gold/1',   x:x-240,y:y-120,type:'bit',clones:150}),
-    client.describe({file:'gold/5',   x:x-080,y:y-120,type:'bit',clones:35}),
-    client.describe({file:'gold/10',  x:x+080,y:y-120,type:'bit',clones:30}),
-    client.describe({file:'gold/50',  x:x+240,y:y-120,type:'bit',clones:15}),
-    client.describe({file:'gold/1',   x:x-240,y:y+120,type:'bit',clones:150}),
-    client.describe({file:'gold/5',   x:x-080,y:y+120,type:'bit',clones:35}),
-    client.describe({file:'gold/10',  x:x+080,y:y+120,type:'bit',clones:30}),
-    client.describe({file:'gold/50',  x:x+240,y:y+120,type:'bit',clones:15}),
+    client.describe({ file: 'gold/1', x: x - 240, y: y - 120, type: 'bit', clones: 150 }),
+    client.describe({ file: 'gold/5', x: x - 80, y: y - 120, type: 'bit', clones: 35 }),
+    client.describe({ file: 'gold/10', x: x + 80, y: y - 120, type: 'bit', clones: 30 }),
+    client.describe({ file: 'gold/50', x: x + 240, y: y - 120, type: 'bit', clones: 15 }),
+    client.describe({ file: 'gold/1', x: x - 240, y: y + 120, type: 'bit', clones: 150 }),
+    client.describe({ file: 'gold/5', x: x - 80, y: y + 120, type: 'bit', clones: 35 }),
+    client.describe({ file: 'gold/10', x: x + 80, y: y + 120, type: 'bit', clones: 30 }),
+    client.describe({ file: 'gold/50', x: x + 240, y: y + 120, type: 'bit', clones: 15 })
   ])
-  return(descriptions)
+  return (descriptions)
 }
 
-const describeCourt = (x,y) => {
-  let descriptions = [] 
+const describeCourt = (x, y) => {
+  let descriptions = []
   descriptions = descriptions.concat([
-    client.describe({file:'board/court',x:x-200,y:0,type:'board'}),
-    client.describe({file:'card/front',x:x-200,y:y,type:'card',cardId:1})
+    client.describe({ file: 'board/court', x: x - 200, y: 0, type: 'board' }),
+    client.describe({ file: 'card/front', x: x - 200, y: y, type: 'card', cardId: 1 })
   ])
-  return(descriptions)
+  return (descriptions)
 }
 
-const annotate = function(description) {
+const annotate = function (description) {
   description.details = ''
-  if(description.type=='card') {
-    var plot = plots[description.cardId]
+  if (description.type == 'card') {
+    let plot = plots[description.cardId]
     description.details = `
       <b>${plot.title}</b><br><br>
       ${plot.effect1}<br><br>
@@ -122,53 +122,53 @@ const annotate = function(description) {
       <a href="${plot.link2}" target="_blank">${plot.link2}</a><br><br>
       `
   }
-  switch( description.file ) {
+  switch (description.file) {
     case 'stack/discard':
       description.details = 'Discard'
-      break;
+      break
     case 'stack/deck':
       description.details = 'Deck'
-      break;
+      break
   }
 }
 
-const compareFunction = (a,b) => {
+const compareFunction = (a, b) => {
   aLayer = 0
   bLayer = 0
-  if(a.type=='board') { aLayer = 1 }
-  if(b.type=='board') { bLayer = 1 }
-  if(a.type=='card') { aLayer = 2 }
-  if(b.type=='card') { bLayer = 2 }
-  if(a.type=='bit') { aLayer = 3 }
-  if(b.type=='bit') { bLayer = 3 }
-  if(a.type=='screen') { aLayer = 4 }
-  if(b.type=='screen') { bLayer = 4 }
-  return aLayer-bLayer
+  if (a.type == 'board') { aLayer = 1 }
+  if (b.type == 'board') { bLayer = 1 }
+  if (a.type == 'card') { aLayer = 2 }
+  if (b.type == 'card') { bLayer = 2 }
+  if (a.type == 'bit') { aLayer = 3 }
+  if (b.type == 'bit') { bLayer = 3 }
+  if (a.type == 'screen') { aLayer = 4 }
+  if (b.type == 'screen') { bLayer = 4 }
+  return aLayer - bLayer
 }
 
 const setup = msg => {
   plots = msg.plots
   console.log(plots)
   let descriptions = []
-  origins.map((origin,i) => { 
+  origins.map((origin, i) => {
     x = origin[0]
     y = origin[1]
-    const portfolio = describePortfolio(x,y,i) 
+    const portfolio = describePortfolio(x, y, i)
     descriptions = descriptions.concat(portfolio)
   })
-  deck = [...Array(51).keys()].filter(x=>x==0||x>11)
+  deck = [...Array(51).keys()].filter(x => x == 0 || x > 11)
   deck = shuffle(deck)
-  auctionRow = deck.slice(0,timelineLength)
+  auctionRow = deck.slice(0, timelineLength)
   auctionRow.sort()
-  descriptions = descriptions.concat(describeBank(2000,0))
-  descriptions = descriptions.concat(describeCourt(-2000,0))
-  for(i of Array(timelineLength).keys()) {
-    var offset = timelineLength/2-0.5
+  descriptions = descriptions.concat(describeBank(2000, 0))
+  descriptions = descriptions.concat(describeCourt(-2000, 0))
+  for (i of Array(timelineLength).keys()) {
+    let offset = timelineLength / 2 - 0.5
     descriptions = descriptions.concat([
-      client.describe({file:'card/front',x:0+(i-offset)*150,y:0,type:'card',cardId:auctionRow[i]})
+      client.describe({ file: 'card/front', x: 0 + (i - offset) * 150, y: 0, type: 'card', cardId: auctionRow[i] })
     ])
   }
   descriptions.map(x => annotate(x))
   descriptions.sort(compareFunction)
-  client.start(descriptions,msg)
+  client.start(descriptions, msg)
 }
--- public/smartdrag.js
+++ public/smartdrag.js
@@ -1,4 +1,4 @@
-/* global Snap */
+/* global Snap, layers, setSide */
 
 const detailDiv = document.getElementById('detail')
 
@@ -15,46 +15,42 @@ Snap.plugin(function (Snap, Element, Paper, global) {
   }
 
   const isFrozen = bit => {
-    readyButtons = bit.parent().children().filter(x => x.data('file') == 'board/ready')
-    frozen = false
+    const readyButtons = bit.parent().children().filter(x => x.data('file') === 'board/ready')
+    let frozen = false
     readyButtons.forEach(button => {
-      if (button.data('side') != 'front') {
-        screens = bit.parent().children().filter(x => x.data('file') == 'board/screen' && x.data('player') == button.data('player'))
-        screens.forEach(screen => frozen = frozen || intersect(bit, screen))
+      if (button.data('side') !== 'front') {
+        const screens = bit.parent().children().filter(x => x.data('file') === 'board/screen' && x.data('player') === button.data('player'))
+        frozen = screens.some(screen => intersect(bit, screen))
       }
     })
     return (frozen)
   }
 
   const isInStack = function (a) {
-    stacks = a.parent().children().filter(bit => ['deck', 'discard'].includes(bit.data('type')))
-    inStack = false
+    const stacks = a.parent().children().filter(bit => ['deck', 'discard'].includes(bit.data('type')))
+    let inStack = false
     stacks.forEach(stack => { inStack = inStack || intersect(a, stack) })
     return inStack
   }
 
   const getMyDiscard = function (deck) {
-    discards = deck.parent().children().filter(bit => bit.data('type') == 'discard')
-    return discards.filter(discard => discard.data('targetDeck') == deck.data('deckId'))[0]
+    const discards = deck.parent().children().filter(bit => bit.data('type') === 'discard')
+    return discards.filter(discard => discard.data('targetDeck') === deck.data('deckId'))[0]
   }
 
   const getMyDeck = function (discard) {
-    decks = discard.parent().children().filter(bit => bit.data('type') == 'deck')
-    return decks.filter(deck => deck.data('deckId') == discard.data('targetDeck'))[0]
+    const decks = discard.parent().children().filter(bit => bit.data('type') === 'deck')
+    return decks.filter(deck => deck.data('deckId') === discard.data('targetDeck'))[0]
   }
 
   const getContents = function (stack) {
-    cards = stack.parent().children().filter(bit => bit.data('type') == 'card')
-    myCards = cards.filter(card => intersect(stack, card))
-    return (myCards)
-  }
-
-  const isEmpty = function (deck) {
-    return getContents(deck).length == 0
+    const cards = stack.parent().children().filter(bit => bit.data('type') === 'card')
+    const myCards = cards.filter(card => intersect(stack, card))
+    return myCards
   }
 
   const getDetails = function (bit) {
-    text = ''
+    let text = ''
     if (!bit.data('twoSided') || ['front', 'hidden'].includes(bit.data('side'))) {
       text = bit.data('details')
     } else {
@@ -66,46 +62,46 @@ Snap.plugin(function (Snap, Element, Paper, global) {
   const dragStart = function (x, y, event) {
     const move = event.button === 0 && !isFrozen(this) && !shiftDown && !ctrlDown && ['card', 'bit'].includes(this.data('type'))
     let flip = event.button === 0 && shiftDown && this.data('twoSided')
-    flip = flip || event.button === 1 && this.data('twoSided')
-    flip = flip || this.data('type') == 'screen'
+    flip = flip || (event.button === 1 && this.data('twoSided'))
+    flip = flip || this.data('type') === 'screen'
     const turnDown = event.button === 0 && ctrlDown && this.data('twoSided')
-    inStack = isInStack(this)
+    const inStack = isInStack(this)
     this.data('inStack', inStack)
     detailDiv.innerHTML = getDetails(this)
-    if (this.data('type') == 'card' & !inStack) {
-      bringToTop(this)
+    if (this.data('type') === 'card' & !inStack) {
+      window.bringToTop(this)
     }
-    if (this.data('file') == 'board/nametag') {
-      const name = prompt('Please enter your name')
+    if (this.data('file') === 'board/nametag') {
+      const name = window.prompt('Please enter your name')
       const children = this.children()
       const textbox = children[children.length - 1]
       console.log(textbox.attr({ text: name }))
       this.data('moved', true)
     }
-    if (this.data('type') == 'discard') {
-      myDeck = getMyDeck(this)
-      myCards = getContents(this)
+    if (this.data('type') === 'discard') {
+      const myDeck = getMyDeck(this)
+      const myCards = getContents(this)
       myCards.sort((a, b) => layers[b.data('id')] - layers[a.data('id')])
       myCards.forEach((card, i) => {
         x = 0.5 * (myDeck.getBBox().width - card.getBBox().width)
         y = -0.5 * (myDeck.getBBox().height - card.getBBox().height) + 50
         card.transform(myDeck.transform().string + 't' + x + ',' + y)
-        bringToTop(card)
+        window.bringToTop(card)
         setSide(card, 'facedown')
         card.data('inStack', true)
         card.data('moved', true)
       })
     }
-    if (this.data('type') == 'deck') {
+    if (this.data('type') === 'deck') {
       console.log('flipDeck')
-      myDiscard = getMyDiscard(this)
-      myCards = getContents(this)
+      const myDiscard = getMyDiscard(this)
+      const myCards = getContents(this)
       myCards.sort((a, b) => layers[b.data('id')] - layers[a.data('id')])
       myCards.forEach((card, i) => {
         x = 0.5 * (myDiscard.getBBox().width - card.getBBox().width)
         y = -0.5 * (myDiscard.getBBox().height - card.getBBox().height) + 50
         card.transform(myDiscard.transform().string + 't' + x + ',' + y)
-        bringToTop(card)
+        window.bringToTop(card)
         setSide(card, 'front')
         card.data('inStack', true)
         card.data('moved', true)
@@ -116,7 +112,7 @@ Snap.plugin(function (Snap, Element, Paper, global) {
       this.data('dragging', true)
       this.data('rotating', false)
       this.data('moved', true)
-    } else if (flip || this.data('type') == 'screen') {
+    } else if (flip || this.data('type') === 'screen') {
       window.flipComponent(this)
       this.data('moved', true)
     } else if (turnDown) {
@@ -127,11 +123,11 @@ Snap.plugin(function (Snap, Element, Paper, global) {
 
   const dragMove = function (dx, dy, event, x, y) {
     if (this.data('dragging')) {
-      inStack = isInStack(this)
+      const inStack = isInStack(this)
       if (!inStack) window.bringToTop(this)
       this.data('inStack', inStack)
       this.data('moved', true)
-      if (this.data('type') == 'bit' || this.data('type') == 'card') {
+      if (this.data('type') === 'bit' || this.data('type') === 'card') {
         const snapInvMatrix = this.transform().diffMatrix.invert()
         snapInvMatrix.e = 0
         snapInvMatrix.f = 0
@@ -148,12 +144,12 @@ Snap.plugin(function (Snap, Element, Paper, global) {
 
   this.onkeydown = event => {
     if (event.key === 'Shift') shiftDown = true
-    if (event.key == 'Control') ctrlDown = true
+    if (event.key === 'Control') ctrlDown = true
   }
 
   this.onkeyup = event => {
     if (event.key === 'Shift') shiftDown = false
-    if (event.key == 'Control') ctrlDown = false
+    if (event.key === 'Control') ctrlDown = false
   }
 
   Element.prototype.smartdrag = function () {
