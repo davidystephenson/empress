8b90f1d473db828d683677f898372abc14fbd047
David Y. Stephenson <david@davidystephenson.com>
2022-12-03 19:37:37 +0100

    Port 8080

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ async function updateClients () {
   events = {}
 }
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`)
 })
